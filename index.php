58922c094d64d23f49423606c1a25c77e6063fa9
DILEEP KUSHWAHA <71811440+dileep8090@users.noreply.github.com>
2022-02-11 17:38:18 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
-    <title>Funda Of Web IT</title>
+    <title>Filter data by PRICE</title>
 </head>
 <body>
   <div class="card-header bg-primary">
